f9e6a93d3dda55f5cab23727e737c66a454208ff
Lucas Koch <mail.lucas.koch@gmail.com>
2025-12-18 13:54:28 +0000

    Add missing controller

---
+++ app/Http/Controllers/ApiTokenController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+
+class ApiTokenController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('ApiTokens', [
+            'tokens' => $request->user()->tokens->map(function ($token) {
+                return [
+                    'id' => $token->id,
+                    'name' => $token->name,
+                    'abilities' => $token->abilities,
+                    'last_used_at' => $token->last_used_at,
+                    'created_at' => $token->created_at,
+                ];
+            }),
+            'flash' => [
+                'plainTextToken' => session('plainTextToken'),
+            ],
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name'      => ['required', 'string', 'max:255'],
+            'abilities' => ['array'],
+        ]);
+
+        Log::info($request);
+        Log::info($validated);
+
+        $token = $request->user()->createToken(
+            $validated['name'],
+            $validated['abilities'] ?? ['*']
+        );
+
+
+        return back()->with('plainTextToken', $token->plainTextToken);
+    }
+
+    public function destroy(Request $request, $tokenId)
+    {
+        $request->user()->tokens()->where('id', $tokenId)->delete();
+
+        return back();
+    }
+}
