f3d955bf0e5779768c7f3bbba298d3782761637a
Lucas Koch <mail.lucas.koch@gmail.com>
2026-08-19 13:27:41 +0200

    Add migration for external models

---
+++ database/migrations/2026_08_18_112732_add_external_llm_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('external_llms', function (Blueprint $table) {
+            $table->id();
+            $table->string('token');
+            $table->string('url');
+            $table->string('name');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
